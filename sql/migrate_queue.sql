fe0c988ea0aa5e63cc09fd34bf9b3f0f38a97715
James Allen Juett <jjuett@lobster2.eecs.umich.edu>
2019-06-13 20:20:33 -0400

    bugfix

--- sql/migrate_queue.sql
+++ sql/migrate_queue.sql
@@ -3,7 +3,7 @@ drop table queueAnnouncements;
 create table announcements (
     id int not null auto_increment primary key,
     queueId int not null,
-    message text not null,
+    content text not null,
     ts timestamp not null default CURRENT_TIMESTAMP
 );
 
