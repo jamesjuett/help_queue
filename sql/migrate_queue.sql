9c4df761b52a9a6b157a39b2174b282cc40f4763
James Juett <jjuett@umich.edu>
2020-03-13 23:20:21 -0400

    add appointments table

--- sql/migrate_queue.sql
+++ sql/migrate_queue.sql
@@ -13,3 +13,12 @@ alter table queue add column priority int not null default 0 after description;
 alter table stack add column priority int not null default 0 after description;
 
 CREATE INDEX queueGroups_email ON queueGroups(email);
+
+create table appointments (
+    id int not null auto_increment primary key,
+    queueId int not null,
+    staffEmail varchar(50) not null,
+    studentEmail varchar(50),
+    startTime datetime not null,
+    duration int not null
+);
